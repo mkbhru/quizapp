350f787e052a8978f443206f78dd57c0d154e033
manishkr3 <manishmpib@gmail.com>
2021-03-07 09:32:43 +0530

    chapter 1 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,38 @@
 import 'package:flutter/material.dart';
+import './questions.dart';
 
 void main() => runApp(Myapp());
 
 class Myapp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    // TODO: implement createState
     // throw UnimplementedError();
-    return MyappState();
+    return _MyappState(); //this underscore makes this class private
+    //and unable to access outside the class
   }
 }
 
-class MyappState extends State<Myapp> {
+class _MyappState extends State<Myapp> {
   //we add generic Myapp to
 //setup connections with Myapp class
-  var questionindex = 0;
-  void answerQuestion() {
+  var _questionindex = 0;
+  void _answerQuestion() {
     setState(() {
-      questionindex = questionindex + 1;
-    });
-    print(questionindex);
-  }
+      _questionindex = _questionindex + 1;
+    }); //because of this set state function we can rerender
+    //in every answer
+    print(_questionindex);
+  } //flutter didn't reubilds entire ui again it
+  //is so eficient thet it ony rebuilds therequired thing
+  // or changed thing
 
   @override
   Widget build(BuildContext context) {
     var questions = [
-      'what is your favourite color',
-      'what is your favourite animal',
-      'what is your favourite number',
-      'what is your favourite name',
+      'what is your favourite color!',
+      'what is your favourite animal!',
+      'what is your favourite number!',
+      'what is your favourite name!',
     ];
     return MaterialApp(
       home: Scaffold(
@@ -37,10 +41,12 @@ class MyappState extends State<Myapp> {
         ),
         body: Column(
           children: [
-            Text(questions[questionindex]),
+            Question(
+              questions[_questionindex],
+            ),
             RaisedButton(
               child: Text('Answer 1'),
-              onPressed: answerQuestion,
+              onPressed: _answerQuestion,
             ),
             RaisedButton(
               child: Text('Answer 2'),
@@ -59,60 +65,5 @@ class MyappState extends State<Myapp> {
   }
 }
 
-//old1
-// import 'package:flutter/material.dart';
-
-// void main() => runApp(Myapp());
-
-// class Myapp extends StatelessWidget {
-//   void answerQuestion() {
-//     print('Answer chosen!');
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: Scaffold(
-//         appBar: AppBar(
-//           title: Text('yo My App'),
-//         ),
-//         // body: Text('this is my default text!'),/
-//         // to add multiple widgets we use invisible widgetshere column
-//         body: Column(
-//           children: [
-//             Text('The Question!'),
-//             RaisedButton(
-//               child: Text('Answer 1'),
-//               // onPressed: null, with null flutter automatically disables the button
-//               onPressed:
-//                   answerQuestion, //we used a pointer here instead of a function
-//             ),
-//             RaisedButton(
-//               child: Text('Answer 2'),
-//               onPressed:
-//                   answerQuestion, //instead of passing the function we passes the pointer which tells
-//               //not only pass the result to button but instead run function every time the button pressed
-//             ),
-//             RaisedButton(
-//               child: Text(
-//                   'Answer 3'), //this gives the flutter full control over that function it dont need to depend on the
-//               //function insted it run the whole function whenever it needs it
-//               onPressed: answerQuestion,
-//             ),
-//             RaisedButton(
-//               child: Text('answer 4'),
-//               onPressed: () => print('answer 4 chosen'),
-//             ),
-//             RaisedButton(
-//                 child: Text('answer 5'),
-//                 onPressed: () {//anonymous function
-//                   print('answer 5 chosen');
-//                   print('yo');
-//                 } //if function is long trry out this
-//                 )
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
+//generally we have one widget per file therefore i
+//create question.dart
